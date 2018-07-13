04d145c6f3f1b50f7286669429984ca921567004
nanouwaiwai@icloud.com <nanouwaiwai@icloud.com>
2018-07-13 08:14:46 +0800

    set game list

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,51 +1,48 @@
 <template>
   <div>
-    <div class="title">小游戏列表</div>
-    <ul class="game-list weui-grids">
+    <ul class="game-list ">
       <li v-for="(item,index) in list"
           v-bind:key="index"
           v-on:click="gotoGameDetail(item)"
-          class="game-item  weui-grid">
-        <div class="weui-grid__icon">
-          <img :src="item.icon" class="game-icon ">
+          class="game-item">
+        <div class="game-image">
+          <img :src="item.poster" width="100%" height="">
         </div>
-
-        <div class="game-name weui-grid__label">{{item.name}}</div>
       </li>
-      {{list.name}}
+      <li>
+        <ad unit-id="adunit-0280b5c185c4eef1"></ad>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+  import { request } from '../../api'
+
   export default {
     name: 'list',
     data: {
-      list: [
-        {
-          icon: '../../static/images/catchsecond.png',
-          name: '抓住1秒',
-          target: 'catchsecond'
-        },
-        {
-          icon: '../../static/images/24.png',
-          name: '算24点',
-          target: '24point'
-        },
-        {
-          icon: '../../static/images/qidai.png',
-          name: '敬请期待',
-          target: 'none'
-        }
-      ]
+      list: []
     },
     methods: {
-      gotoGameDetail: function (item) {
-        wx.navigateTo('/catchsecond')
+      gotoGameDetail (item) {
+        wx.previewImage({
+          urls: [item.qr_img] // 需要预览的图片http链接列表
+        })
+      },
+      getGameList () {
+        request({
+          url: 'https://judatree.cn/api/json/game_list.json',
+          method: 'GET'
+        }).then(data => {
+          console.log(data)
+        })
       }
     },
     created () {
+      this.getGameList()
     }
+
   }
 </script>
 
@@ -56,15 +53,13 @@
   .title {
     background-color: #f7f7f7;
     font-size: 28 rpx;
-    padding: 10 rpx;
-  }
-
-  .weui-grid__label {
-    font-size: 30 rpx;
   }
 
   .game-list {
-    color: @font_grey;
+    img {
+      width: 100vw;
+      height: 40vw;
+    }
 
   }
 </style>
